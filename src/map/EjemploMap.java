1fceba1e15cd4bcd8775e6f4a50eb132af107a42
JorgeGaro-esp <h95jorgaro@gmail.com>
2026-02-04 16:20:48 +0000

    .

---
+++ src/map/EjemploMap.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package map;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jorge Garcia Rodriguez
+ */
+public class EjemploMap {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner in = new Scanner(System.in);
+        Map<String, Integer> map = new HashMap<>();
+        System.out.println("Introduce una palabra");
+        
+    
+    }
+
+}
